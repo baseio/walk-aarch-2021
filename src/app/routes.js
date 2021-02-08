cb66477946460b5b626185239c1090e18d9558c4
Jørgen Skogmo <js@dearstudio.dk>
2021-02-08 21:23:28 +0100

    rev3, preview 1. No 'detail' view

--- src/app/routes.js
+++ src/app/routes.js
@@ -5,13 +5,13 @@ let firstLoad = true
 export const handleHash = (rawHash) => {
 
 	let hash = rawHash.replace('#', '')
-	console.log('handleHash:', 'rawHash:', rawHash, 'hash:', hash)
+	// console.log('handleHash:', 'rawHash:', rawHash, 'hash:', hash)
 
 	window.app.userDrawPlaying = false
 
 	// console.log('OnHashChanged #2', app.balls);
 	const b = window.app.balls.filter( b => b.el.userData.data.stub === hash )[0]
-	// console.log('OnHashChanged #3', b);
+	// console.log('handleHash #3', b);
 	
 	// if $graduates is selected, just filter the text-list
 	const inGraduatesMode = document.querySelector(`#sidebar [data-trigger="feat"][data-key="graduates"]`).classList.contains("selected")
@@ -19,7 +19,51 @@ export const handleHash = (rawHash) => {
 
 	if( b ){
 		// student
-		console.log('handleHash: student')
+		console.log('handleHash: student', b)
+
+		// Rev3 ADD
+		// Check if we should hide the project link from this student
+		const stub = b.el.userData.data.stub
+		const sid  = b.el.userData.data.id
+		// console.log('## LINK for:', id, sid, stub)
+		if( DATA.DATA_EXCLUDE_PROJECTLINK.includes( sid ) ){
+			console.log('## DONT OPEN PROJECT LINK for:', sid, stub)
+		}else{
+			const PROJECT_LINK = `https://afgang.aarch.dk/2021/student/${stub}`
+			console.log('## OPEN PROJECT LINK:', PROJECT_LINK)
+			window.open(PROJECT_LINK)
+		}
+
+		document.querySelectorAll(`#sidebar [data-trigger="theme"]`).forEach( (el) => {
+			el.classList.remove('selected')
+		})
+
+		const theme = DATA.THEMES.filter(t => t.id === b.el.userData.data.theme)[0]
+		const btn = document.querySelector(`#sidebar [data-trigger="theme"][data-key="${theme.slug}"]`)
+		if( btn ){
+			btn.classList.add('selected')
+		}
+
+		// window.app.animation.applyFilter('theme', theme.id)		
+
+		window.app.actions.render_theme(theme)
+
+		console.log('firstLoad:', firstLoad);
+		if( firstLoad ){
+			window.toFree()
+			setTimeout( () => {
+				window.app.animation.applyFilter('theme', theme.id)
+			}, 500)
+
+
+			setTimeout( () => {
+				// show tooltip
+				window.app.animation.showTooltip( b.el.position, b.el.userData.data.name )
+			}, 1500)
+
+		}
+
+		/*
 
 		const accepted = window.toNode( b.i )
 		const theme_slug = DATA.THEMES.filter(t => t.id === b.el.userData.data.theme)[0]?.slug
@@ -31,6 +75,7 @@ export const handleHash = (rawHash) => {
 				btn.classList.add('selected')
 			}
 		}
+		*/
 
 	}else if( rawHash.indexOf('#theme:') === 0 ){
 		// theme
